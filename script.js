0e907db875835e1d23609de84dacd50071ead123
jpetz <60827828+j-petz@users.noreply.github.com>
2026-03-12 14:41:02 +0100

    Change color of pixels by random parameters

--- script.js
+++ script.js
@@ -41,5 +41,9 @@ function deleteSquares() {
 }
 
 function changeColor() {
-    this.style.backgroundColor = "lightcoral";
+    //this.style.backgroundColor = "lightcoral";
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    this.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
 }
